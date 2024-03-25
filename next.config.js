d7c7e6661604b3a491b2ad21d2d3535b2a17fc44
heungjun.park <rockheung@gmail.com>
2024-03-25 18:44:12 +0900

    feat: text tw property

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    emotion: true,
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.jsx": [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-react",
+                  { runtime: "automatic", importSource: "@emotion/react" },
+                ],
+              ],
+              plugins: ["babel-plugin-macros", "@emotion/babel-plugin"],
+            },
+          },
+        ],
+      },
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
